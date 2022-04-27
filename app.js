a2445e0ba289448c6a87ecaf052110ed2315c77c
Nico <nico.nap@outlook.com>
2022-04-27 16:04:18 +0200

    Facebook further setup

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
+var session = require('express-session');
 require('dotenv').config();
 const mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
@@ -21,6 +22,47 @@ mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      callbackURL: 'http://localhost:3000/auth/success',
+    },
+    function (accessToken, refreshToken, profile, done) {
+      process.nextTick(function () {
+        User.findOne({ facebookId: profile.id }, function (err, user) {
+          if (err) return done(err);
+          if (user) {
+            return done(null, user);
+          } else {
+            var newUser = new User();
+            newUser.displayName = profile.displayName;
+            newUser.facebookId = profile.id;
+            newUser.token = accessToken;
+            newUser.email = profile.emails[0].value;
+
+            newUser.save(function (err) {
+              if (err) throw err;
+              return done(null, user);
+            });
+          }
+        });
+      });
+    }
+  )
+);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -30,33 +72,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-passport.use(
-  new FacebookStrategy(
-    {
-      clientID: process.env.FACEBOOK_APP_ID,
-      clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: 'http://localhost:3000/auth/facebook/redirect',
-    },
-    function (accessToken, refreshToken, profile, cb) {
-      console.log(profile);
-      if (User.exists({ facebookId: profile.id })) {
-        User.findOne({ facebookId: profile.id }, function (err, user) {
-          if (err) return cb(err);
-          return cb(null, user);
-        });
-      } else {
-        User.create(
-          { displayName: profile.displayName, facebookId: profile.id },
-          function (err, user) {
-            if (err) return cb(err);
-            return cb(null, user);
-          }
-        );
-      }
-    }
-  )
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: false,
+  })
 );
+app.use(passport.authenticate('session'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
