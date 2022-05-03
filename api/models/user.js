d83436162b151431fab1cf353dd601738f8ceede
Nico <nico.nap@outlook.com>
2022-05-03 14:04:15 +0200

    Change user model

--- api/models/user.js
+++ api/models/user.js
@@ -2,11 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-  displayName: { type: String, required: true },
-  facebookId: { type: String, required: true },
-  token: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   email: { type: String, required: true },
-  picture: { type: String, required: false },
   friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   requests: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
