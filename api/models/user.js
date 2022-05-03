8e6be6350bef4aa52839e0dc9728d3bad03ad112
Nico <nico.nap@outlook.com>
2022-05-03 14:06:11 +0200

    Add image field

--- api/models/user.js
+++ api/models/user.js
@@ -5,6 +5,7 @@ var UserSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true },
+  profilePicture: { data: Buffer, contentType: String },
   friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   requests: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
