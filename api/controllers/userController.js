8891e108740d125ca9c853bd4fe7c582e6950cf4
Nico <nico.nap@outlook.com>
2022-05-06 16:20:59 +0200

    Add user update method

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -137,3 +137,107 @@ exports.delete_user = function (req, res, next) {
     );
   });
 };
+
+exports.update_user = [
+  (req, res, next) => {
+    jwt.verify(req.token, process.env.SESSION_SECRET, function (err, authData) {
+      if (err) {
+        res.sendStatus(403);
+        return;
+      } else {
+        req.authData = authData;
+        next();
+      }
+    });
+  },
+
+  body('username', 'Username must be longer than 3 characters')
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+  body('username', 'Username is already taken').custom((value, { req }) => {
+    return new Promise((resolve, reject) => {
+      User.findOne({ username: req.body.username }, function (err, user) {
+        if (err) return next(err);
+        if (
+          user &&
+          user.username == value &&
+          user.username != req.authData.username
+        ) {
+          reject(new Error('Username is already taken'));
+        }
+        resolve(true);
+      });
+    });
+  }),
+  body('firstName', 'First name cannot be empty')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('lastName', 'Last name cannot be empty')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('email', 'E-mail cannot be empty').trim().isLength({ min: 1 }),
+  body('email', 'Enter a valid e-mail').isEmail(),
+  body('password', 'Password must be 8 characters or longer').isLength({
+    min: 8,
+  }),
+
+  (req, res, next) => {
+    async.parallel(
+      {
+        user: function (callback) {
+          User.findById(req.params.id).exec(callback);
+        },
+      },
+      function (err, results) {
+        if (err) return next(err);
+        if (results.user.username != req.authData.username) {
+          res.sendStatus(403);
+          return;
+        }
+        if (results.user == null) {
+          res.sendStatus(404);
+          return;
+        }
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          res.json({ errors: errors.array() });
+          return;
+        } else {
+          bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+            if (err) return next(err);
+            var user = new User({
+              _id: results.user._id,
+              username: req.body.username,
+              firstName: req.body.firstName,
+              lastName: req.body.lastName,
+              email: req.body.email,
+              password: hashedPassword,
+              friends: results.user.friends,
+              requests: results.user.requests,
+              posts: results.user.posts,
+            });
+            User.findByIdAndUpdate(
+              results.user._id,
+              user,
+              { new: true },
+              function (err, theUser) {
+                if (err) return next(err);
+                res.json({
+                  message: 'User has been updated',
+                  user: theUser,
+                  new_token: jwt.sign(
+                    theUser.toJSON(),
+                    process.env.SESSION_SECRET
+                  ),
+                });
+              }
+            );
+          });
+        }
+      }
+    );
+  },
+];
--- api/routes/user.js
+++ api/routes/user.js
@@ -2,14 +2,19 @@ const express = require('express');
 var router = express.Router();
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
-const { get_user, delete_user } = require('../controllers/userController');
+const {
+  get_user,
+  delete_user,
+  update_user,
+} = require('../controllers/userController');
 
 router.get('/:id', verifyToken, get_user);
 
 router.delete('/:id', verifyToken, delete_user);
 
+router.put('/:id', verifyToken, update_user);
+
 function verifyToken(req, res, next) {
-  console.log('verify');
   const bearerHeader = req.headers['authorization'];
   if (typeof bearerHeader != 'undefined') {
     const bearer = bearerHeader.split(' ')[1];
