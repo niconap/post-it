4bf798f808c0e924e10080aef6d68cc0ddcadb80
Nico Nap <nico.nap@outlook.com>
2022-07-05 08:04:08 +0200

    Finish login component

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,18 +1,56 @@
-import { useEffect } from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
-  useEffect(() => {
-    fetch('http://localhost:5000/api/user/')
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  }, []);
+  let navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    try {
+      let res = await fetch('http://localhost:5000/api/auth/login', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      });
+      let resJson = await res.json();
+      if (resJson.token) {
+        setUsername('');
+        setPassword('');
+        localStorage.setItem('token', resJson.token);
+        navigate('../', { replace: true });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div id="login">
-      <form onsubmit="return false;" method="get" name="login">
-        <input type="text" name="username" placeholder="Username" />
-        <input type="password" name="password" placeholder="Password" />
-        <input type="submit" value="Log in" onclick={validate(event)} />
+      <form onSubmit={handleSubmit} method="get" name="login">
+        <input
+          type="text"
+          value={username}
+          name="username"
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          name="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input type="submit" value="Log in" />
       </form>
       <p>
         Don't have an account? Sign up <a href="signup">here</a>
