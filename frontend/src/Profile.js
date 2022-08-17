938939ab1d3533e5f88762494ca3705418ff5788
Nico <nico.nap@outlook.com>
2022-08-17 17:57:09 +0200

    Add localhost routes

--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -11,17 +11,20 @@ function Profile(props) {
   const [requests, setRequests] = useState([]);
 
   const fetchUserData = async () => {
-    let res = await fetch(`/api/user/get/${searchParams.get('id')}`, {
-      method: 'GET',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: localStorage.getItem('token'),
-      },
-    });
+    let res = await fetch(
+      `http://localhost:5000/api/user/get/${searchParams.get('id')}`,
+      {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: localStorage.getItem('token'),
+        },
+      }
+    );
     let resJson = await res.json();
     setUser(resJson);
-    let res2 = await fetch(`/api/user/requests`, {
+    let res2 = await fetch(`http://localhost:5000/api/user/requests`, {
       method: 'GET',
       mode: 'cors',
       headers: {
@@ -40,7 +43,7 @@ function Profile(props) {
 
   const handleRemoveFriend = async () => {
     try {
-      await fetch(`/api/user/friend/remove/${user._id}/`, {
+      await fetch(`http://localhost:5000/api/user/friend/remove/${user._id}/`, {
         method: 'PUT',
         mode: 'cors',
         headers: {
@@ -59,7 +62,7 @@ function Profile(props) {
     const formData = new FormData();
     formData.append('image', selectedImage);
     try {
-      await fetch(`/api/user/picture`, {
+      await fetch(`http://localhost:5000/api/user/picture`, {
         method: 'POST',
         mode: 'cors',
         headers: {
