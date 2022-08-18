6ba39ae42fcbea6bc2bc8e7fdb880fec78de7d8b
Nico <nico.nap@outlook.com>
2022-08-18 11:47:20 +0200

    Add form validation to commentform

--- frontend/src/CommentForm.js
+++ frontend/src/CommentForm.js
@@ -1,23 +1,34 @@
 import { useState } from 'react';
+import uniqid from 'uniqid';
 
 function CommentForm(props) {
   const [commentContent, setCommentContent] = useState('');
+  const [errors, setErrors] = useState([]);
 
   let handleSubmit = async (e) => {
+    setErrors([]);
     e.preventDefault();
     try {
-      await fetch(`http://localhost:5000/api/post/${props.postId}/comment`, {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: localStorage.getItem('token'),
-        },
-        body: JSON.stringify({
-          content: commentContent,
-        }),
-      });
-      props.getPost();
+      let res = await fetch(
+        `http://localhost:5000/api/post/${props.postId}/comment`,
+        {
+          method: 'POST',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: localStorage.getItem('token'),
+          },
+          body: JSON.stringify({
+            content: commentContent,
+          }),
+        }
+      );
+      let resJson = await res.json();
+      if (resJson.errors) {
+        setErrors(resJson.errors);
+      } else {
+        props.getPost();
+      }
     } catch (err) {
       console.log(err);
     }
@@ -26,7 +37,15 @@ function CommentForm(props) {
   return (
     <div id="commentinput">
       <form name="comment" onSubmit={handleSubmit}>
+        <span
+          className={`counter${
+            commentContent.length > 250 ? ' redcounter' : ''
+          }`}
+        >
+          {commentContent.length} / 250
+        </span>
         <input
+          className={`${commentContent.length > 250 ? 'redinput' : ''}`}
           type="text"
           placeholder="Type a comment..."
           value={commentContent}
@@ -36,6 +55,15 @@ function CommentForm(props) {
         />
         <input type="submit" value="Post" className="submit" />
       </form>
+      <ul>
+        {errors.map((error) => {
+          return (
+            <li className="inputerror" key={uniqid()}>
+              {error.msg}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
